5448c7b49a1d2cec2a395140b2c63b4b4d62d52e
Salar Rahmanian <code@softinio.com>
2018-12-16 15:05:29 -0800

    Upgraded dependencies

--- pymagecli/handler.py
+++ pymagecli/handler.py
@@ -12,3 +12,4 @@ def lambda_handler(event):
         bucket_name = unquote_plus(object['s3']['object']['bucket_name'])
         print("Pymage: Resizing {} in bucket: {}".format(key, bucket_name))
         get_file_and_resize(bucket_name=bucket_name, file_name=key)
+
